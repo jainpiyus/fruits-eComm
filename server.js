4cfc89c053445a328cdb3f9cdb9834da8ba10e99
Piyush Jain <Piyush.Jain@nice.com>
2020-02-26 21:45:56 +0530

    add server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/e-comm'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname, '/dist/e-comm/index.html'));
+    res.sendFile(path.join(__dirname + './front-end/dist/e-comm/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
