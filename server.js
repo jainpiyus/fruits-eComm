daae1bee138694948ec6e6682265032d3046b2b3
Piyush Jain <Piyush.Jain@nice.com>
2020-02-26 20:43:29 +0530

    add

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/e-comm'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname, '/dist/e-comm/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/e-comm/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Server started");
+})
